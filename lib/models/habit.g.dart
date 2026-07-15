ac1d53c35a114a52e7fe500f23a45ce6a89af9c2
lemon6243 <lemon6243@lsyesco.co.kr>
2026-07-15 10:50:49 +0900

    Add generated habit.g.dart

---
+++ lib/models/habit.g.dart
@@ -0,0 +1,59 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'habit.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class HabitAdapter extends TypeAdapter<Habit> {
+  @override
+  final int typeId = 3;
+
+  @override
+  Habit read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return Habit(
+      id: fields[0] as String,
+      name: fields[1] as String,
+      emoji: fields[2] as String,
+      colorValue: fields[3] as int?,
+      checkedDates: (fields[4] as List?)?.cast<String>(),
+      createdAt: fields[5] as DateTime,
+      isArchived: fields[6] as bool,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, Habit obj) {
+    writer
+      ..writeByte(7)
+      ..writeByte(0)
+      ..write(obj.id)
+      ..writeByte(1)
+      ..write(obj.name)
+      ..writeByte(2)
+      ..write(obj.emoji)
+      ..writeByte(3)
+      ..write(obj.colorValue)
+      ..writeByte(4)
+      ..write(obj.checkedDates)
+      ..writeByte(5)
+      ..write(obj.createdAt)
+      ..writeByte(6)
+      ..write(obj.isArchived);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is HabitAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
