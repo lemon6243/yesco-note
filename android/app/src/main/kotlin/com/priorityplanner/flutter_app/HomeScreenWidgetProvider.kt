4144daed64e0a08dbc206966e31b07c78fadf72a
lemon6243 <93980574+lemon6243@users.noreply.github.com>
2026-07-22 22:57:57 +0900

    Create HomeScreenWidgetProvider.kt

---
+++ android/app/src/main/kotlin/com/priorityplanner/flutter_app/HomeScreenWidgetProvider.kt
@@ -0,0 +1,29 @@
+package com.example.flutter_app // 본인의 패키지명으로 꼭 변경해주세요!
+
+import android.appwidget.AppWidgetManager
+import android.content.Context
+import android.content.SharedPreferences
+import android.widget.RemoteViews
+import es.antonborri.home_widget.HomeWidgetProvider
+
+class HomeScreenWidgetProvider : HomeWidgetProvider() {
+    override fun onUpdate(
+        context: Context,
+        appWidgetManager: AppWidgetManager,
+        appWidgetIds: IntArray,
+        widgetData: SharedPreferences
+    ) {
+        for (appWidgetId in appWidgetIds) {
+            // 방금 만든 레이아웃 파일을 연결
+            val views = RemoteViews(context.packageName, R.layout.widget_layout)
+
+            // 플러터(Dart)에서 'widget_task_list'라는 키로 저장한 글자를 불러옴
+            val tasksText = widgetData.getString("widget_task_list", "오늘의 할 일이 없습니다! 🎉")
+            
+            // 레이아웃의 widget_task_text 아이디를 가진 텍스트뷰에 글자를 입력
+            views.setTextViewText(R.id.widget_task_text, tasksText)
+
+            appWidgetManager.updateAppWidget(appWidgetId, views)
+        }
+    }
+}
